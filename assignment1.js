f86f9042ad96fddb96f42c80a5fe2bbdc4ac6c8e
Clinton6788 <clinton.g.hockenberry@gmail.com>
2024-08-25 19:17:05 -0400

    First commit

---
+++ assignment1.js
@@ -0,0 +1,21 @@
+// ASSIGNMENT 1
+
+let storyName = "John";
+let physicalAction = "squat";
+let companyName = "coding coders llc";
+let dailyAction = "making coffee";
+let noun = "dog";
+
+const codingLanguage = "python"
+
+//Version 2
+storyName = "Greg";
+physicalAction = "yawn";
+companyName = "McDonalds";
+dailyAction = "driving"
+noun = "paper towels"
+
+console.log("There was once a coder named " + storyName + ".")
+console.log(storyName + " liked to " + physicalAction + " every morning before starting work at " + companyName + ".")
+console.log("His/her favorite part of the day was " + dailyAction + ", especially when dealing with " + codingLanguage + ".")
+console.log("However, his/her " + noun + " always seemed to get in the way when using " + codingLanguage + ".")
